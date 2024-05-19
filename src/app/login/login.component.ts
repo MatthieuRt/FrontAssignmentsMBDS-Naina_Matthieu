0d03e56bb94f90ff15eaa12f4680c5b6c970b2e7
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-05-19 09:16:25 +0300

    Amelioration affichage login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() { }
+
+  login() {
+    //logique de connexion
+    console.log(this.email)
+    console.log(this.password)
+  }
 }
