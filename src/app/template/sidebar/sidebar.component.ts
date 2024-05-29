8c10317fdf40d70454d1a9079484e47f02e97147
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-05-29 19:08:53 +0300

    Modification navbar
    - affichage selon role
    - ajout du menu dashboard pour le prof

--- src/app/template/sidebar/sidebar.component.ts
+++ src/app/template/sidebar/sidebar.component.ts
@@ -11,12 +11,20 @@ import { RouterModule } from '@angular/router'
 export class SidebarComponent {
   user:any;
   assigmentRouteLabel:string='Tous les assignments ';
+  isProf : boolean = false;
+  isAdmin : boolean = false;
   constructor(){
     const userstring: string | null = localStorage.getItem("USER");
     if (userstring) {
       this.user = JSON.parse(userstring);
       if(this.user.role=='eleve'){
         this.assigmentRouteLabel = 'Vos assignments'
+      }
+      if(this.user.role=='prof'){
+        this.isProf=true
+      }
+      if(this.user.role=='admin'){
+        this.isAdmin = true;
       }
     }
   }
