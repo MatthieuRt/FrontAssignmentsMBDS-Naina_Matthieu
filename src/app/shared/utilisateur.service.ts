a24533bbabf66bd1455e2b0b6c7e316b1d8127db
NainaMatthieu <andrianmattax@gmail.com>
2024-05-30 21:40:17 +0300

    Final commit

--- src/app/shared/utilisateur.service.ts
+++ src/app/shared/utilisateur.service.ts
@@ -55,7 +55,7 @@ export class UtilisateurService {
         }
 
         return forkJoin(
-          matieres.map(matiere => 
+          matieres.map(matiere =>
             this.getUserById(matiere.professeur_id).pipe(
               map(prof => ({
                 ...matiere,
@@ -81,49 +81,58 @@ export class UtilisateurService {
     );
   }
   getAssignmentByIdStudent_IdMatiere(idMatiere: string | null): Observable<Assignment> {
-    // let idEtudiant =  sessionStorage.getItem("idEtudiant");
-    let idEtudiant = "663a52b9946fa30b7711db7d";
-    // const url = this.uri + "/assignments/663a52b9946fa30b7711db7d/66433f5b0c3e8e917d4e9a6a"
-    const url = this.uri + "users/assignments/" + idEtudiant + "/" + idMatiere;
-    this._assignmentStudent.next(null);
-    console.log(url)
-    return this.http.get<any>(url).pipe(
-      map((reponse) => {
-        this._assignmentStudent.next(reponse);
-        return reponse;
-      }),
-      switchMap((assignments) => {
+    const userstring: string | null = localStorage.getItem("USER");
 
-        if (!assignments) {
-          return throwError('Il n\'y a pas d\'assignment pour {idEtudiant:  ' + idEtudiant + ',idMatiere: ' + idMatiere);
-        }
+    if (userstring) {
+      const user = JSON.parse(userstring);
+      const idEtudiant = user._id;
+      const url = `${this.uri}users/assignments/${idEtudiant}/${idMatiere}`;
 
-        return of(assignments);
-      })
-    );
+      this._assignmentStudent.next(null);
+      console.log(url);
+
+      return this.http.get<any>(url).pipe(
+        map((reponse) => {
+          this._assignmentStudent.next(reponse);
+          return reponse;
+        }),
+        switchMap((assignments) => {
+          if (!assignments) {
+            return throwError(`Il n'y a pas d'assignment pour {idEtudiant: ${idEtudiant}, idMatiere: ${idMatiere}}`);
+          }
+          return of(assignments);
+        }),
+        catchError((error) => {
+          console.error('Error fetching assignments:', error);
+          return throwError('Erreur lors de la récupération des assignments');
+        })
+      );
+    } else {
+      return throwError('Utilisateur non trouvé dans le localStorage');
+    }
   }
-  getAssignmentByIdStudent(idEtudiant:string,page: number, limit: number): Observable<any> {
+  getAssignmentByIdStudent(idEtudiant: string, page: number, limit: number): Observable<any> {
     //let idEtudiant = "663a52b9946fa30b7711db7d";
     const matiereList: any = [];
     const matiereSet = new Set<string>(); // Utilisation d'un ensemble pour suivre les matières demandées
     let count = 0;
     const url = `${this.uri}users/assignments/${idEtudiant}?page=${page}&limit=${limit}`;
-  
+
     return this.http.get<any>(url).pipe(
       mergeMap((response: any) => {
         const listAssignment = response.docs;
         const reponse = response;
-  
+
         const assignmentPromises = listAssignment.map(async (assignment: any) => {
           let matiereIN = matiereList.find((mat: any) => mat._id === assignment.idMatiere);
-  
+
           if (!matiereIN && !matiereSet.has(assignment.idMatiere)) {
             count += 1;
             console.log("Appel à getMatiereById : " + count);
             matiereSet.add(assignment.idMatiere); // Ajout de l'ID de la matière à l'ensemble
-  
-            const matiereResponse :any= await this.getMatiereById(assignment.idMatiere).toPromise();
-  
+
+            const matiereResponse: any = await this.getMatiereById(assignment.idMatiere).toPromise();
+
             // On s'assure qu'il n'y a pas de doublons après avoir récupéré la réponse
             if (!matiereList.some((mat: any) => mat._id === matiereResponse._id)) {
               matiereList.push(matiereResponse);
@@ -131,7 +140,7 @@ export class UtilisateurService {
             } else {
               console.log("Doublon détecté et évité :", matiereResponse);
             }
-  
+
             assignment.matiere = matiereResponse.Matiere;
             assignment.matiere_img = matiereResponse.image;
             assignment.prof_img = matiereResponse.prof_img;
@@ -142,10 +151,10 @@ export class UtilisateurService {
             assignment.prof_img = matiereIN.prof_img;
             assignment.prof_id = matiereIN.professeur_id;
           }
-  
+
           return assignment;
         });
-  
+
         // Utilisation de Promise.all pour attendre toutes les promesses des assignments
         return from(Promise.all(assignmentPromises)).pipe(
           map((updatedAssignments) => {
@@ -161,18 +170,18 @@ export class UtilisateurService {
     const url = this.uri + "matiere/" + idMatiere;
     return this.http.get<any>(url);
   }
-  getListEtudiants(): Observable<any>{
+  getListEtudiants(): Observable<any> {
     const url = this.uri + "eleves";
     return this.http.get<any>(url);
   }
-  rendreAssignments(assignments:any): Observable<any>{
+  rendreAssignments(assignments: any): Observable<any> {
     const url = `${this.uri}user/rendre`;
     const data = {
-      listAssignment : assignments
+      listAssignment: assignments
     }
-    return this.http.post<any>(url,data);
+    return this.http.post<any>(url, data);
   }
-  getAllAssignments(page: number, limit: number): Observable<any>{
+  getAllAssignments(page: number, limit: number): Observable<any> {
     const matiereList: any = [];
     const matiereSet = new Set<string>(); // Utilisation d'un ensemble pour suivre les matières demandées
     let count = 0;
@@ -181,17 +190,17 @@ export class UtilisateurService {
       mergeMap((response: any) => {
         const listAssignment = response.docs;
         const reponse = response;
-  
+
         const assignmentPromises = listAssignment.map(async (assignment: any) => {
           let matiereIN = matiereList.find((mat: any) => mat._id === assignment.idMatiere);
-  
+
           if (!matiereIN && !matiereSet.has(assignment.idMatiere)) {
             count += 1;
             console.log("(ALL ASIGN)Appel à getMatiereById : " + count);
             matiereSet.add(assignment.idMatiere); // Ajout de l'ID de la matière à l'ensemble
-  
-            const matiereResponse :any= await this.getMatiereById(assignment.idMatiere).toPromise();
-  
+
+            const matiereResponse: any = await this.getMatiereById(assignment.idMatiere).toPromise();
+
             // On s'assure qu'il n'y a pas de doublons après avoir récupéré la réponse
             if (!matiereList.some((mat: any) => mat._id === matiereResponse._id)) {
               matiereList.push(matiereResponse);
@@ -199,7 +208,7 @@ export class UtilisateurService {
             } else {
               console.log("(ALL ASIGN) Doublon détecté et évité :", matiereResponse);
             }
-  
+
             assignment.matiere = matiereResponse.Matiere;
             assignment.matiere_img = matiereResponse.image;
             assignment.prof_img = matiereResponse.prof_img;
@@ -210,10 +219,10 @@ export class UtilisateurService {
             assignment.prof_img = matiereIN.prof_img;
             assignment.prof_id = matiereIN.professeur_id;
           }
-  
+
           return assignment;
         });
-  
+
         // Utilisation de Promise.all pour attendre toutes les promesses des assignments
         return from(Promise.all(assignmentPromises)).pipe(
           map((updatedAssignments) => {
